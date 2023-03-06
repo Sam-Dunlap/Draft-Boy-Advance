8193cea282ce404224402845780b58f5b7522b89
Sam-Dunlap <sam.dunlap15@gmail.com>
2023-03-05 19:59:29 -0800

    no wake up message

--- events/ready.js
+++ events/ready.js
@@ -4,9 +4,6 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		const channelCache = client.channels.cache
-		const mainChannel = channelCache.find(channel => channel.name === 'general');
-		mainChannel.send(':wave:');
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
